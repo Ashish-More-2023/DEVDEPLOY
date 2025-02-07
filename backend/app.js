f78e3f2e01441e7de316e049ecbe7889c4cd791c
Ashish-More-2023 <ashishmoreee123@gmail.com>
2025-02-08 00:47:33 +0530

    asd

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,9 @@ app.use(
   })
 );
 
-
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -68,9 +70,7 @@ app.get('/search',async (req,res)=>{
     }
 })
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
