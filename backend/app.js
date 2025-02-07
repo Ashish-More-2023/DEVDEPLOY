867544732f59450d3d16ac3d2d3d299afe51bd4f
Ashish-More-2023 <ashishmoreee123@gmail.com>
2025-02-08 01:28:37 +0530

    ads

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin:process.env.CLIENT_URL || "http://localhost:3000",
+    origin:process.env.CLIENT_URL || "http://localhost:3000" || "https://devdeploy-h0ml07b5a-ashish-s-projects-f9743213.vercel.app/auth/login/success" || "https://devdeploy-frontend.vercel.app",
     methods:"GET,POST,PUT,DELETE",
     credentials:true,
   })
